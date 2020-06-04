3a64e2ab87f6c713017addf3f0f07a2807c5d0ed
Ragnhild Gya <Ragnhild.Gya@uib.no>
2020-06-04 12:55:43 +0200

    Adding extra cleaning code for new data. Pluss making many new plots

--- Germination_Clean_data.R
+++ Germination_Clean_data.R
@@ -4,12 +4,14 @@
 
 library(tidyverse)
 library(lubridate)
+#library(extrafont)
+#loadfonts(device = "win")
 
 ## Load data ##
 
-VA_germ <-read.csv2("Data/INCLINE_Germination_Seedling_Experiment_Data_2020_04_03_VA.csv", header=TRUE, sep = ";", stringsAsFactors = FALSE)
-SP_germ <-read.csv2("Data/INCLINE_Germination_Seedling_Experiment_Data_2020_04_03_SP.csv", header=TRUE, sep = ";", stringsAsFactors = FALSE)
-
+VA_germ <- read.csv2("Data/INCLINE_Germination_Seedling_Experiment_Data_2020_05_15_VA.csv", header=TRUE, sep = ",", stringsAsFactors = FALSE)
+SP_germ <-read.csv2("Data/INCLINE_Germination_Seedling_Experiment_Data_2020_05_15_SP.csv", header=TRUE, sep = ",", dec = ".", stringsAsFactors = FALSE)
+ 
 
 ## Veronical alpina ##
 
@@ -19,15 +21,19 @@ VA_mistakes <- read.table(header = TRUE, stringsAsFactors = FALSE, text =
   02.02.2020 02.03.2020
   20.2.20 20.02.2020
   20.2.2020 20.02.2020
-  21.02 21.02.2020")
+  21.02 21.02.2020
+  13.04 13.04.2020 
+  21.02.2002 20.02.2020
+  04.02.2020 04.03.2020")
 
 
 
 VA_germ1 <- VA_germ %>%
-  filter(!Leaf_date == " ") %>% 
+  filter(!Leaf_date == " ",
+         !Germination_date %in% c("DEAD", "dead")) %>% 
   mutate(Germination_date = plyr::mapvalues(Germination_date, from = VA_mistakes$old, to = VA_mistakes$new),
-         Cotelydon_date = plyr::mapvalues(Cotelydon_date, from = SP_mistakes$old, to = SP_mistakes$new),
-         Leaf_date = plyr::mapvalues(Leaf_date, from = SP_mistakes$old, to = SP_mistakes$new)) %>% 
+         Cotelydon_date = plyr::mapvalues(Cotelydon_date, from = VA_mistakes$old, to = VA_mistakes$new),
+         Leaf_date = plyr::mapvalues(Leaf_date, from = VA_mistakes$old, to = VA_mistakes$new)) %>% 
   mutate(Start_date = dmy(Start_date)) %>% 
   mutate(Germination_date = dmy(Germination_date)) %>% 
   mutate(Cotelydon_date = dmy(Cotelydon_date)) %>% 
@@ -54,7 +60,23 @@ VA_germ_analysis <- VA_germ1 %>%
   ungroup() %>% 
   group_by(Petri_dish) %>% 
   arrange(Days_to_germination) %>% 
-  mutate(Cum_germ_percent = cumsum(Germination_percent))
+  mutate(Cum_germ_percent = cumsum(Germination_percent)) %>% 
+  ungroup() %>% 
+  group_by(Species, Site, Water_potential, Days_to_germination) %>% 
+  mutate(Average = mean(Cum_germ_percent)) %>% 
+  ungroup() %>% 
+  mutate(Days_to_germination = as.numeric(Days_to_germination))
+
+Last_day_germination_score <- VA_germ_analysis %>% 
+  group_by(Petri_dish) %>% 
+  mutate(Max_germination = max(Cum_germ_percent)) %>% 
+  select(Max_germination, Petri_dish, Species, Site, Water_potential, Replicate) %>% 
+  mutate(Days_to_germination = 76)
+
+VA_germ_analysis <- VA_germ_analysis %>% 
+  bind_rows(Last_day_germination_score) %>% 
+  mutate(Cum_germ_percent = ifelse(is.na(Cum_germ_percent), Max_germination, Cum_germ_percent))
+
 
 VA_cotelydon_analysis <- VA_germ1 %>% 
   filter(!is.na(Cotelydon_date)) %>% 
@@ -121,7 +143,23 @@ SP_germ_analysis <- SP_germ1 %>%
   ungroup() %>% 
   group_by(Petri_dish) %>% 
   arrange(Days_to_germination) %>% 
-  mutate(Cum_germ_percent = cumsum(Germination_percent))
+  mutate(Cum_germ_percent = cumsum(Germination_percent)) %>% 
+  ungroup() %>% 
+  group_by(Species, Site, Water_potential, Days_to_germination) %>% 
+  mutate(Average = mean(Cum_germ_percent)) %>% 
+  ungroup() %>% 
+  mutate(Days_to_germination = as.numeric(Days_to_germination))
+
+Last_day_germination_score_SP <- SP_germ_analysis %>% 
+  group_by(Petri_dish) %>% 
+  mutate(Max_germination = max(Cum_germ_percent)) %>% 
+  select(Max_germination, Petri_dish, Species, Site, Water_potential, Replicate) %>% 
+  mutate(Days_to_germination = 76)
+
+SP_germ_analysis <- SP_germ_analysis %>% 
+  bind_rows(Last_day_germination_score_SP) %>% 
+  mutate(Cum_germ_percent = ifelse(is.na(Cum_germ_percent), Max_germination, Cum_germ_percent))
+
 
 SP_cotelydon_analysis <- SP_germ1 %>% 
   filter(!is.na(Cotelydon_date)) %>% 
@@ -153,7 +191,7 @@ WP_names <- c(
   "7" = "WP 7 (-0.95 MPa)",
   "8" = "WP 8 (-1.20 MPa)",
   "9" = "WP 9 (-1.45 MPa)",
-  "10" = "WP 10 (-1.70 MPa"
+  "10" = "WP 10 (-1.70 MPa)"
 )
 
 ## Function for plots ##
@@ -163,9 +201,36 @@ Plot_germination_graph <- function(data, site, species){
   data <- data %>% 
     filter(Site == site)
   
-  plot <- ggplot(aes(x = sort(as.integer(Days_to_germination)), y = Cum_germ_percent, color = Replicate), data = data) +
-    geom_point() +
+  plot <- ggplot(aes(x = sort(Days_to_germination), y = Cum_germ_percent), data = data) +
+    geom_point(aes(color = Replicate, alpha = 0.01)) +
+    geom_line(aes(color = Replicate, alpha = 0.01)) +
+    stat_smooth(method = loess, formula = x~y^2, color = "black", size = 3) +
+    facet_wrap(~ Water_potential, labeller = as_labeller(WP_names)) +
+    xlab("Days to germination") +
+    ylab("Germination %") +
+    ggtitle(paste(species, "from", site)) +
+    theme(plot.title = element_text(hjust = 0.5),
+          panel.background = element_rect(fill = "white",
+                                          colour = "white",
+                                          size = 0.5,
+                                          linetype = "solid"),
+          panel.grid.major = element_line(size = 0.25,
+                                          linetype = 'solid',
+                                          colour = "lightgrey")) +
+    scale_x_continuous(limits = c(0,80)) +
+    scale_y_continuous(limits = c(0,100))
+  
+  return(plot)
+}
+
+Plot_germination_mean_graph <- function(data, site, species){
+  
+  data <- data %>% 
+    filter(Site == site)
+  
+  plot <- ggplot(aes(x = sort(as.integer(Days_to_germination)), y = Average), data = data) +
     geom_line() +
+    geom_point(Cum_germ_percent, alpha = 0.1) +
     facet_wrap(~ Water_potential, labeller = as_labeller(WP_names)) +
     xlab("Days to germination") +
     ylab("Germination %") +
@@ -215,7 +280,7 @@ Plot_leaf_graph <- function(data, site, species){
   data <- data %>% 
     filter(Site == site)
   
-  plot <- ggplot(aes(x = sort(as.integer(Days_to_leaf)), y = Cum_leaf_percent, color = Replicate), data = data) +
+  plot <- ggplot(aes(x = sort(as.integer(Days_to_leaf)), y = Cum_leaf_percent), data = data) +
     geom_point() +
     geom_line() +
     facet_wrap(~ Water_potential, labeller = as_labeller(WP_names)) +
@@ -252,6 +317,12 @@ Plot_germination_graph(data = VA_germ_analysis, site = "LAV", species = "Veronic
 
 Plot_germination_graph(data = VA_germ_analysis, site = "ULV", species = "Veronica alpina")
 #ggsave("VA_ULV.png", width = 15, height = 13, unit = "cm")
+
+
+# VA germination average #
+
+Plot_germination_mean_graph(data = VA_germ_analysis, site = "SKJ", species = "Veronica alpina")
+#ggsave("VA_SKJ.png", width = 15, height = 10, unit = "cm")
 
 
 # VA cotelydon data #
@@ -303,48 +374,107 @@ Plot_cotelydon_graph(data = SP_cotelydon_analysis, site = "SKJ", species = "Sibb
 
 
 
-ggplot(aes(x = Water_potential, y = Days_since_germination), data = SP_germ1) +
-  geom_boxplot()
+## SP biomass plots ##
 
-ggplot(aes(x = Water_potential, y = Days_since_leaf), data = SP_germ1) +
-  geom_boxplot()
+Seedling_info_SP <- SP_germ1 %>%
+  mutate(AGB_mass_g = Wet_mass_g_rest + Wet_mass_g_True.leaf,
+         Total.wet.mass = Wet_mass_g_root + Wet_mass_g_rest + Wet_mass_g_True.leaf,
+         rs_ratio = Wet_mass_g_root/AGB_mass_g) %>% 
+  filter(!rs_ratio > 2,
+         !Total.wet.mass >0.010)
 
-ggplot(aes(x = Water_potential, y = Days_since_leaf), data = VA_germ1) +
-  geom_jitter( alpha = 0.1) +
-  geom_violin()
+#Mass total plot
 
-ggplot(aes(x = Water_potential, y = Days_since_leaf), data = SP_germ1) +
-  geom_violin() +
-  geom_jitter(alpha = 0.1)
+ggplot(aes(x = Water_potential, y = Total.wet.mass), data = Seedling_info_SP) +
+  geom_violin(aes(fill = Water_potential)) +
+  geom_jitter(alpha = 0.3) +
+  scale_fill_brewer(palette = "Blues", direction = -1) +
+  xlab("Water potential") +
+  ylab("Total wet mass (g)") +
+  theme(text = element_text(size=20),
+        legend.position = "none")
 
-VA_leaf <- VA_germ1 %>% 
-  filter(!is.na(Leaf_date)) %>% 
-  ungroup() %>% 
-  mutate(Days_since_leaf = as.numeric(Days_since_leaf)) %>% 
-  group_by(Site, Water_potential, Replicate, Days_since_leaf) %>%
-  mutate(n = n()) %>%
-  select(Site, Water_potential, Replicate, Days_since_leaf, n) %>%
-  distinct() %>% 
-  spread(Days_since_leaf, n)
-
-write_csv(VA_leaf, path = "VA_leaf_summary.csv")
+ggplot(aes(x = Water_potential, y = rs_ratio), data = Seedling_info_SP) +
+  geom_violin(aes(fill = Water_potential)) +
+  geom_jitter(alpha = 0.3) +
+  scale_fill_brewer(palette = "Blues", direction = -1) +
+  xlab("Water potential") +
+  ylab("Root/shoot ratio") +
+  theme(text = element_text(size=20),
+        legend.position = "none")
 
 
-SP_germ1 %>% 
-  filter(!is.na(Leaf_date)) %>% 
-  ungroup() %>% 
-  count()
+# Germination percent plots
 
-SP_leaf <- SP_germ1 %>% 
-  filter(!is.na(Leaf_date)) %>% 
-  ungroup() %>% 
-  mutate(Days_since_leaf = as.numeric(Days_since_leaf)) %>% 
-  group_by(Site, Water_potential, Replicate, Days_since_leaf) %>%
-  mutate(n = n()) %>%
-  filter(!Wet_mass_g_root == "") %>% 
-  select(Site, Water_potential, Replicate, Days_since_leaf, n) %>%
-  distinct() %>% 
-  spread(Days_since_leaf, n)
+VA_germ_analysis %>% 
+  mutate(Site = factor(Site, levels = c("SKJ", "GUD", "LAV", "ULV"))) %>% 
+ggplot(aes(x = Water_potential, y = Max_germination)) +
+  geom_boxplot() +
+  facet_wrap(~Site, nrow = 4) + 
+  xlab("Max germination percent (%)") +
+  ylab("Water potential") +
+  theme(text = element_text(size=20, family = "Century Schoolbook"))
 
-write_csv(SP_leaf, path = "SP_leaf_summary_with_traitleaves.csv")
+SP_germ_analysis %>% 
+  filter(Site == "LAV") %>% 
+ggplot(aes(x = Water_potential, y = Max_germination)) +
+  geom_boxplot() +
+  xlab("Max germination percent (%)") +
+  ylab("Water potential") +
+  theme(text = element_text(size=20))
 
+
+#Richards suggestion to plot, but this does not account for the fact that they have different levels of 100 % gemrination. Some seeds have not germinated yet, although everything goes to 100 %.
+
+# VA_germ_analysis %>% 
+#   ggplot(aes(as.numeric(Days_to_germination), color = Water_potential)) +
+#   stat_ecdf() +
+#   facet_wrap(~ Site)
+
+
+#Plots and info used to decide when to sample germinants
+
+# ggplot(aes(x = Water_potential, y = Days_since_germination), data = SP_germ1) +
+#   geom_boxplot()
+# 
+# ggplot(aes(x = Water_potential, y = Days_since_leaf), data = SP_germ1) +
+#   geom_boxplot()
+# 
+# ggplot(aes(x = Water_potential, y = Days_since_leaf), data = VA_germ1) +
+#   geom_jitter( alpha = 0.1) +
+#   geom_violin()
+# 
+# ggplot(aes(x = Water_potential, y = Days_since_leaf), data = SP_germ1) +
+#   geom_violin() +
+#   geom_jitter(alpha = 0.1)
+# 
+# VA_leaf <- VA_germ1 %>% 
+#   filter(!is.na(Leaf_date)) %>% 
+#   ungroup() %>% 
+#   mutate(Days_since_leaf = as.numeric(Days_since_leaf)) %>% 
+#   group_by(Site, Water_potential, Replicate, Days_since_leaf) %>%
+#   mutate(n = n()) %>%
+#   select(Site, Water_potential, Replicate, Days_since_leaf, n) %>%
+#   distinct() %>% 
+#   spread(Days_since_leaf, n)
+# 
+# write_csv(VA_leaf, path = "VA_leaf_summary.csv")
+# 
+# 
+# SP_germ1 %>% 
+#   filter(!is.na(Leaf_date)) %>% 
+#   ungroup() %>% 
+#   count()
+# 
+# SP_leaf <- SP_germ1 %>% 
+#   filter(!is.na(Leaf_date)) %>% 
+#   ungroup() %>% 
+#   mutate(Days_since_leaf = as.numeric(Days_since_leaf)) %>% 
+#   group_by(Site, Water_potential, Replicate, Days_since_leaf) %>%
+#   mutate(n = n()) %>%
+#   filter(!Wet_mass_g_root == "") %>% 
+#   select(Site, Water_potential, Replicate, Days_since_leaf, n) %>%
+#   distinct() %>% 
+#   spread(Days_since_leaf, n)
+# 
+# write_csv(SP_leaf, path = "SP_leaf_summary_with_traitleaves.csv")
